0e992fc3a32e6c5ba4545cf1234f4829b893c1ae
lds92 <luciad@student.chalmers.se>
2017-02-20 16:47:31 +0100

    new net with squeeze

--- meowNET/main.py
+++ meowNET/main.py
@@ -5,19 +5,20 @@ import os
 import time
 
 TRAIN = True
-LEARNING_RATE = 0.01
+#LEARNING_RATE = 0.001
+INITIAL_LEARNING_RATE=0.001
 NR_ITERATIONS = 10000
 PRINT_FREQ = 100
 BATCH_SIZE = 128
 NO_CLASSES = 10
-PATH_TO_IMAGES = "/Users/Donal/Dropbox/CIFAR10/Data/train/images/"
-PATH_TO_LABELS = "/Users/Donal/Dropbox/CIFAR10/Data/train/labels.txt"
-PATH_TO_TEST_IMAGES = "/Users/Donal/Dropbox/CIFAR10/Data/test/images/"
-PATH_TO_LOGS = "/Users/Donal/Desktop/"
-#PATH_TO_IMAGES = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/train/images/"
-#PATH_TO_LABELS = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/train/labels.txt"
-#PATH_TO_TEST_IMAGES = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/test/images/"
-#PATH_TO_LOGS = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/meow_logs/"
+#PATH_TO_IMAGES = "/Users/Donal/Dropbox/CIFAR10/Data/train/images/"
+#PATH_TO_LABELS = "/Users/Donal/Dropbox/CIFAR10/Data/train/labels.txt"
+#PATH_TO_TEST_IMAGES = "/Users/Donal/Dropbox/CIFAR10/Data/test/images/"
+#PATH_TO_LOGS = "/Users/Donal/Desktop/"
+PATH_TO_IMAGES = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/train/images/"
+PATH_TO_LABELS = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/train/labels.txt"
+PATH_TO_TEST_IMAGES = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/Data/test/images/"
+PATH_TO_LOGS = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/meow_logs/"
 cwd = os.getcwd()
 
 # build input graph
@@ -25,15 +26,17 @@ batch_size = tf.placeholder(dtype=tf.int32)
 batch = input.create_batch(PATH_TO_IMAGES, PATH_TO_LABELS, batch_size, TRAIN)
 x = batch[0]
 y_ = tf.one_hot(batch[1], NO_CLASSES, dtype=tf.int32)
-
+global_step=tf.Variable(0,trainable= False)
 # build CNN graph
-h_pool3 = nf.meow_net(x)
+h_pool3 = nf.squeeze_net(x)
 
 if TRAIN:
 
     # build training graph
+    learning_rate = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step,
+                                               8000, 0.5, staircase=True)
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=h_pool3, labels=y_))
-    train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy,global_step=global_step)
     correct_prediction = tf.equal(tf.argmax(h_pool3, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     summary_op = tf.summary.scalar("Accuracy", accuracy)
@@ -55,8 +58,9 @@ if TRAIN:
     for i in range(NR_ITERATIONS):
         if i % PRINT_FREQ == 0:
             # evaluate forward pass for mini-batch
-            train_accuracy, summary = sess.run([accuracy, summary_op], feed_dict={batch_size: BATCH_SIZE})
+            train_accuracy, lr, summary = sess.run([accuracy,learning_rate, summary_op], feed_dict={batch_size: BATCH_SIZE})
             print("step %d, train accuracy = %g, time taken = %g seconds" % (i, train_accuracy, time.clock()-start_time))
+            print("learning rate = %g" % (lr))
             # write accuracy to log file
             summary_writer.add_summary(summary, i)
             start_time = time.clock()
--- meowNET/network_function.py
+++ meowNET/network_function.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 KEEP_PROP = 0.5
 
@@ -19,6 +20,10 @@ def conv2d(x, W):
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='VALID')
+
+def max_pool_3x3(x):
+    return tf.nn.max_pool(x, ksize=[1, 3, 3, 1],
                         strides=[1, 2, 2, 1], padding='VALID')
 
 def max_pool_8x8(x):
@@ -149,4 +154,64 @@ def deep_dropout_meow_net(x):
     h_pool4 = tf.squeeze(h_pool4)
 
     return h_pool4
+
+def squeeze_net(x):
+
+    W_conv1 = weight_variable([3, 1,3,32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(tf.nn.conv2d(x, W_conv1, strides=[1, 2, 2, 1], padding='VALID') + b_conv1)
+
+    W_conv2=weight_variable([1,3,32,32])
+    b_conv2=bias_variable([32])
+    h_conv2=tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+
+    h_pool1 = max_pool_3x3(h_conv2)
+
+    h_fire1=fire(h_pool1,32,s1x1=16,e1x1=32,e3x1=32)
+    h_fire2=fire(h_fire1,64,s1x1=16,e1x1=32,e3x1=32)
+    h_pool2=max_pool_3x3(h_fire2)
+
+    h_fire3=fire(h_pool2,64,s1x1=16,e1x1=64,e3x1=64)
+    h_fire4=fire(h_fire3,128,s1x1=16,e1x1=64,e3x1=64)
+    h_pool3=max_pool_3x3(h_fire4)
+
+
+    h_fire5=fire(h_pool3,128,s1x1=32,e1x1=128,e3x1=128)
+    #h_fire6 = fire(h_fire5, 384, s1x1=48, e1x1=192, e3x1=192)
+    #h_fire7 = fire(h_fire6, 384, s1x1=64, e1x1=256, e3x1=256)
+    #h_fire8 = fire(h_fire7, 512, s1x1=64, e1x1=256, e3x1=256)
+    #h_fire9 = fire(h_fire8, 512, s1x1=96, e1x1=384, e3x1=384)
+    #h_fire10 = fire(h_fire9, 768, s1x1=96, e1x1=384, e3x1=384)
+    # dropout
+    #keep_prop = tf.constant(KEEP_PROP, dtype=tf.float32)
+    #h_drop = tf.nn.dropout(h_fire5, keep_prop)
+    W_conv3 = weight_variable([3, 3,256,10])
+    b_conv3 = bias_variable([10])
+    h_conv3 = tf.nn.bias_add(conv2d(h_fire5, W_conv3), b_conv3)
+    h_conv3 = tf.squeeze(h_conv3)
+    return h_conv3
+
+
+def fire(x,input_depth,s1x1,e1x1,e3x1):
+
+    #Squeeze layer
+    W_s = weight_variable([1, 1, input_depth, s1x1])
+    b_s = bias_variable([s1x1])
+    h_s = tf.nn.relu(conv2d(x, W_s) + b_s)
+
+    #Expand Layer
+    W_e1x1=weight_variable([1, 1, s1x1, e1x1])
+    b_e1x1 = bias_variable([e1x1])
+    h_e1x1=tf.nn.bias_add(conv2d(h_s,W_e1x1),b_e1x1)
+    h_e1x1=tf.nn.relu(h_e1x1)
+
+    W_e3x1=weight_variable([3,1 , s1x1, e3x1])
+    b_e3x1 = bias_variable([e3x1])
+    h_e3x1=tf.nn.bias_add(conv2d(h_s,W_e3x1),b_e3x1)
+    W_e1x3=weight_variable([1,3, e3x1, e3x1])
+    b_e1x3 = bias_variable([e3x1])
+    h_e1x3=tf.nn.bias_add(conv2d(h_e3x1,W_e1x3),b_e1x3)
+    h_e1x3=tf.nn.relu(h_e1x3)
+
+    return tf.concat([h_e1x1, h_e1x3],3)
 
