4e5533495705c0552f95cb4ceec8f935189d05cc
lds92 <luciad@student.chalmers.se>
2017-02-23 11:44:54 +0100

    testing

--- guauNET/loss.py
+++ guauNET/loss.py
@@ -20,7 +20,7 @@ def bbox_transform_inv(bbox):
 
 def calculate_loss(network_output,y_):
     with tf.name_scope('Loss'):
-
+        #lala
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=network_output, labels=y_),
                                        name='CrossEntropy')
         correct_prediction = tf.equal(tf.argmax(network_output, 1), tf.argmax(y_, 1))
