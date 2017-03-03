cdf4b1e82e71b68207d37058baf7fe1b65c838e9
lds92 <luciad@student.chalmers.se>
2017-03-03 10:10:26 +0100

    anchors added

--- guauNET/parameters.py
+++ guauNET/parameters.py
@@ -1,3 +1,4 @@
+import numpy as np
 TRAIN = True
 LEARNING_RATE = 0.001
 EPSILON = 0.0001
@@ -11,7 +12,7 @@ OUTPUT_WIDTH = 25
 OUTPUT_HEIGHT = 6
 NR_ANCHORS_PER_CELL = 9
 CLASSES = {'Car': '0', 'Van': '1', 'Truck': '2', 'Pedestrian': '3', 'Person_sitting': '4', 'Cyclist': '5', 'Tram': '6', 'Misc': '7', 'DontCare': '8'}
-USER = 'DONAL'
+USER = 'LUCIA'
 if USER == 'DONAL':
     PATH_TO_IMAGES = "/Users/Donal/Dropbox/KITTI/test/image/"
     PATH_TO_LABELS = "/Users/Donal/Dropbox/KITTI/test/label/"
@@ -28,3 +29,41 @@ else:
     PATH_TO_LOGS = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/meow_logs/"
     PATH_TO_TEST_OUTPUT = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/test_output/"
     PATH_TO_CKPT = "C:/Master Chalmers/2 year/volvo thesis/code0/MEOW/checkpoints/"
+
+def set_anchors():
+  H, W, B = 22, 76, 9
+  anchor_shapes = np.reshape(
+      [np.array(
+          [[  36.,  37.], [ 366., 174.], [ 115.,  59.],
+           [ 162.,  87.], [  38.,  90.], [ 258., 173.],
+           [ 224., 108.], [  78., 170.], [  72.,  43.]])] * H * W,
+      (H, W, B, 2)
+  )
+  center_x = np.reshape(
+      np.transpose(
+          np.reshape(
+              np.array([np.arange(1, W+1)*float(IMAGE_WIDTH)/(W+1)]*H*B),
+              (B, H, W)
+          ),
+          (1, 2, 0)
+      ),
+      (H, W, B, 1)
+  )
+  center_y = np.reshape(
+      np.transpose(
+          np.reshape(
+              np.array([np.arange(1, H+1)*float(IMAGE_HEIGHT)/(H+1)]*W*B),
+              (B, W, H)
+          ),
+          (2, 1, 0)
+      ),
+      (H, W, B, 1)
+  )
+  anchors = np.reshape(
+      np.concatenate((center_x, center_y, anchor_shapes), axis=3),
+      (-1, 4)
+  )
+
+  return anchors
+
+ANCHORS=set_anchors()
