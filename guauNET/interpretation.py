1b448449a648c24ef9575e7e94c6e3cd25c4fc7f
Donal <scanlan@student.chalmers.se>
2017-03-06 11:42:03 +0100

    Wee changes to interp

--- guauNET/interpretation.py
+++ guauNET/interpretation.py
@@ -7,8 +7,8 @@ def interpret(network_output):
     Args:
         network_output: a 4d tensor outputted from the CNN [batch_sz,height,width,depth]
     Return:
-        class_scores: a 3d tensor containing the Pr(Cl|Obj) dist. for each anchor [batch_sz, no_anchors_per_image, no_classes]
-        confidence_scores: a 2d tensor containing the Pr(Obj)*IOU for each anchor [batch_sz, no_anchors_per_image]
+        class_scores: a 3d tensor containing the Conf(Cl|Obj) dist. for each anchor [batch_sz, no_anchors_per_image, no_classes]
+        confidence_scores: a 2d tensor containing the Conf(Obj)*IOU for each anchor [batch_sz, no_anchors_per_image]
         bbox_deltas: a 3d tensor containing the parameterised offsets for each anchor [batch_sz, no_anchors_per_image,4]
     '''
     with tf.name_scope('Interpretation'):
@@ -21,7 +21,7 @@ def interpret(network_output):
                         [-1, p.NR_CLASSES]
                     )
                 ),
-                [p.BATCH_SIZE, p.NR_ANCHORS_PER_CELL*p.OUTPUT_HEIGHT*p.OUTPUT_WIDTH, p.NR_CLASSES],
+                [p.BATCH_SIZE, p.NR_ANCHORS_PER_IMAGE, p.NR_CLASSES],
                 name='ClassScores'
             )
 
@@ -30,7 +30,7 @@ def interpret(network_output):
             confidence_scores = tf.sigmoid(
                 tf.reshape(
                     network_output[:, :, :, num_class_probs:num_confidence_scores],
-                    [p.BATCH_SIZE, p.NR_ANCHORS_PER_CELL*p.OUTPUT_HEIGHT*p.OUTPUT_WIDTH]
+                    [p.BATCH_SIZE, p.NR_ANCHORS_PER_IMAGE]
                 ),
                 name='ConfidenceScores'
             )
@@ -38,7 +38,7 @@ def interpret(network_output):
         with tf.name_scope('ReformatBboxDelta'):
             bbox_delta = tf.reshape(
                 network_output[:, :, :, num_confidence_scores:],
-                [ p.BATCH_SIZE, p.NR_ANCHORS_PER_CELL*p.OUTPUT_HEIGHT*p.OUTPUT_WIDTH, 4],
+                [ p.BATCH_SIZE, p.NR_ANCHORS_PER_IMAGE, 4],
                 name='bboxDelta'
             )
         return class_scores, confidence_scores, bbox_delta
