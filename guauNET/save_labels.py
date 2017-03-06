ad2cadc7be425885eb960d8bf695a0e7583f0655
lds92 <luciad@student.chalmers.se>
2017-03-06 09:27:28 +0100

    new label file

--- guauNET/save_labels.py
+++ guauNET/save_labels.py
@@ -6,6 +6,7 @@ import tools as t
 
 from parameters import IMAGE_HEIGHT, IMAGE_WIDTH, CLASSES, ANCHORS, NR_ANCHORS_PER_IMAGE, NR_CLASSES
 
+
 # TO DO!
 # GT_MASK: 1 if anchor has max IOU with GT [batch size, no anchors per image]
 #               - compute IOU between each anchor and each GT
@@ -14,19 +15,6 @@ from parameters import IMAGE_HEIGHT, IMAGE_WIDTH, CLASSES, ANCHORS, NR_ANCHORS_P
 # GT_COORDS: coordinates of GT bbox assigned to each anchor [batch size, no anchors per image, 4]
 # GT_LABELS: labels of GT assigned to each anchor [batch size, no anchors per image, no classes]
 # GT_OBJ_PER_IMAGE: number of objects per image [batch size]
-
-def read_image(filename):
-    """
-    Args:
-        filename: a scalar string tensor.
-    Returns:
-        image_tensor: decoded image
-    """
-
-    file_contents = tf.read_file(filename)
-    image = tf.image.decode_png(file_contents, channels=3)
-    image = tf.image.resize_images(image, [IMAGE_HEIGHT, IMAGE_WIDTH])
-    return image
 
 def read_labels(path_to_labels):
     """
@@ -40,7 +28,7 @@ def read_labels(path_to_labels):
     bboxes = []
     classes = []
     index_images = []
-    i=0
+    i = 0
     for file in [path_to_labels + s for s in label_list]:
         data = open(file, 'r').read()
         # separate the values for each image, every image can have more than one object
@@ -50,7 +38,7 @@ def read_labels(path_to_labels):
         for obj in range(0, len(line), 15):
             # extract ground truth coordinates [x,y,w,h] and class for each object
             annot = line[obj + 0]
-            cls = int(CLASSES[annot]) # CLASSES convert classes to a number, eg. 'Pedestrian' to 3
+            cls = int(CLASSES[annot])  # CLASSES convert classes to a number, eg. 'Pedestrian' to 3
             xmin = float(line[4 + obj])
             ymin = float(line[5 + obj])
             xmax = float(line[6 + obj])
@@ -59,36 +47,24 @@ def read_labels(path_to_labels):
             # create a new classes and bboxes lists, if first object create new list
             if obj == 0:
                 classes.append([cls])
-                bboxes.append([(x,y,w,h)])
+                bboxes.append([(x, y, w, h)])
             else:
-                ind=index_images.index(i)
+                ind = index_images.index(i)
                 classes[ind].append(cls)
-                bboxes[ind].append((x,y,w,h))
-        i=i+1
-    return classes,bboxes
-
-
-def create_image_list(path_to_images):
-    """
-    Args:
-        path_to_images: full path to image folder
-    Returns:
-        image_list: a tensor of all files in that folder
-    """
-
-    image_list = os.listdir(path_to_images)
-    image_list = [path_to_images + s for s in image_list]
-    return image_list
+                bboxes[ind].append((x, y, w, h))
+        i = i + 1
+    return classes, bboxes
 
 def compute_deltas(coords):
     coords = np.array(coords)
     anchors = np.array(ANCHORS)
-    delta_x = (coords[:,0] - anchors[:,0])/anchors[:,2]
-    delta_y=(coords[:,1]-anchors[:,1])/anchors[:,3]
-    delta_w=np.log(anchors[:,2]/anchors[:,2])
-    delta_h=np.log(anchors[:,3]/anchors[:,3])
-    deltas=np.transpose([delta_x, delta_y, delta_w, delta_h])
+    delta_x = (coords[:, 0] - anchors[:, 0]) / anchors[:, 2]
+    delta_y = (coords[:, 1] - anchors[:, 1]) / anchors[:, 3]
+    delta_w = np.log(anchors[:, 2] / anchors[:, 2])
+    delta_h = np.log(anchors[:, 3] / anchors[:, 3])
+    deltas = np.transpose([delta_x, delta_y, delta_w, delta_h])
     return deltas
+
 
 def assign_gt_to_anchors(classes, bboxes):
     """
@@ -112,14 +88,14 @@ def assign_gt_to_anchors(classes, bboxes):
         mask[anchor_idx_for_obj] = 1
         im_coords = bboxes[image_idx]
         coords = [im_coords[i] for i in obj_idx_for_anchor[:]]
-        deltas=compute_deltas(coords)
-        im_label=classes[image_idx]
-        pre_label=np.array([im_label[i] for i in obj_idx_for_anchor[:]])
-        label=np.zeros((NR_ANCHORS_PER_IMAGE,NR_CLASSES))
-        label[np.arange(NR_ANCHORS_PER_IMAGE),pre_label]=1
+        deltas = compute_deltas(coords)
+        im_label = classes[image_idx]
+        pre_label = np.array([im_label[i] for i in obj_idx_for_anchor[:]])
+        label = np.zeros((NR_ANCHORS_PER_IMAGE, NR_CLASSES))
+        label[np.arange(NR_ANCHORS_PER_IMAGE), pre_label] = 1
 
 
-def create_batch(path_to_images, path_to_labels, batch_size, train):
+def create_label(path_to_images, path_to_labels):
     """
     Args:
         path_to_images: full path to input images folder
@@ -130,19 +106,7 @@ def create_batch(path_to_images, path_to_labels, batch_size, train):
         batch: list of images as a 4d tensor sz = [batch_sz, im_h, im_w, im_d]
         and labels as a 1d tensor sz = [batch_sz]
     """
-    image_list = create_image_list(path_to_images)
-    no_samples = len(image_list)
-    image_list = tf.convert_to_tensor(image_list, dtype=tf.string)
-    if train:
-       classes, bboxes = read_labels(path_to_labels)
-       gt_mask, gt_deltas, gt_coords, gt_labels = assign_gt_to_anchors(classes, bboxes)
-    else:   # Create fake labels for testing data
-        classes = [0]*no_samples
-        bboxes = [0]*no_samples
-    classes = tf.convert_to_tensor(classes, dtype=tf.int32)
-    bboxes = tf.convert_to_tensor(bboxes, dtype=tf.float32)
-    input_queue = tf.train.slice_input_producer([image_list, classes, bboxes], shuffle=False)
-    images = read_image(input_queue[0])
-    batch = tf.train.batch([images, input_queue[1], input_queue[2]], batch_size=batch_size)
-    return batch
+
+    classes, bboxes = read_labels(path_to_labels)
+    assign_gt_to_anchors(classes, bboxes)
 
