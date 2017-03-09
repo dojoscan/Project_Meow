ceaa5ec698c9f5f8acd1fa564568f6170081cfaf
lds92 <luciad@student.chalmers.se>
2017-03-09 09:38:31 +0100

    train script

--- guauNET/train.py
+++ guauNET/train.py
@@ -1,31 +1,46 @@
 # TRAINING PIPELINE FOR KITTI
 
 import tensorflow as tf
-import network_function as nf
+import network as net
 import kitti_input as ki
-import loss
+import parameters as p
+import interpretation as interp
+import loss as l
 import os
 import time
-import parameters as p
+import numpy as np
 
 cwd = os.getcwd()
 
 # build input graph
 with tf.name_scope('InputPipeline'):
     batch_size = tf.placeholder(dtype=tf.int32, name='BatchSize')
-    batch = ki.create_batch(p.PATH_TO_IMAGES, p.PATH_TO_LABELS, batch_size, p.TRAIN)
+    batch = ki.create_batch(batch_size, p.TRAIN)
     x = batch[0]
-    y_ = batch[1]
+    gt_mask = batch[1]
+    gt_deltas = batch[2]
+    gt_coords = batch[3]
+    gt_labels = batch[4]
 
 # build CNN graph
-network_output = nf.squeeze_net(x)
+network_output = net.squeeze_net(x)
+
+# build interpretation graph
+class_scores, confidence_scores, bbox_delta = interp.interpret(network_output)
 
 # build loss graph
-loss, train_accuracy = loss.calculate_loss(network_output, y_)
+total_loss, bbox_loss, confidence_loss, classification_loss = l.loss_function(gt_mask, gt_deltas, gt_coords,  bbox_delta, confidence_scores, gt_labels, class_scores)
 
 # build training graph
-with tf.name_scope('Training'):
-    train_step = tf.train.AdamOptimizer(p.LEARNING_RATE, name='LearningRate').minimize(loss, name='CrossEntropy')
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.exponential_decay(p.LEARNING_RATE, global_step,
+                                           p.DECAY_STEP, p.DECAY_FACTOR, staircase=True)
+train_step = tf.train.AdamOptimizer(learning_rate)
+grads_vars = train_step.compute_gradients(total_loss, tf.trainable_variables())
+for i, (grad, var) in enumerate(grads_vars):
+    grads_vars[i] = (tf.clip_by_norm(grad, 1), var)
+apply_gradient_op = train_step.apply_gradients(grads_vars, global_step=global_step)
+
 
 merged_summaries = tf.summary.merge_all()
 
@@ -46,14 +61,17 @@ print('Training initiated')
 start_time = time.clock()
 for i in range(p.NR_ITERATIONS):
     if i % p.PRINT_FREQ == 0:
-        # run inference graph
-        batch_loss, summary = sess.run([loss, merged_summaries], feed_dict={batch_size: p.BATCH_SIZE})
-        print("step %d, train accuracy = %g, time taken = %g seconds" % (i, batch_loss, time.clock()-start_time))
+        # evaluate loss for mini-batch
+        final_loss, b1, conf1, class1, summary = sess.run([total_loss, bbox_loss, confidence_loss, classification_loss, merged_summaries],
+                                                 feed_dict={batch_size: p.BATCH_SIZE})
+        print("step %d, train loss = %g, time taken = %g seconds" % (i, final_loss, time.clock() - start_time))
+        print("Bbox loss = %g, Confidence loss = %g, Class loss = %g" % (b1, conf1, class1))
+        print("----------------------------------------------------------------------------")
         # write accuracy to log file
         summary_writer.add_summary(summary, i)
         start_time = time.clock()
     # run training graph
-    sess.run(train_step, feed_dict={batch_size: p.BATCH_SIZE})
+    sess.run(apply_gradient_op, feed_dict={batch_size: p.BATCH_SIZE})
 
 save_path = saver.save(sess, p.PATH_TO_CKPT)
 print("Model saved in file: %s" % p.PATH_TO_CKPT)
