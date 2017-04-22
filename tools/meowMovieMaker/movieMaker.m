99306fcd0f6d22ed79d7c682d927f467841d2b7b
Donal <scanlan@student.chalmers.se>
2017-04-22 14:50:46 +0200

    Val fix in oink

--- tools/meowMovieMaker/movieMaker.m
+++ tools/meowMovieMaker/movieMaker.m
@@ -1,21 +1,22 @@
-path_to_images = 'C:\Users\Donal\Dropbox\KITTI\data\testing\image\';
-path_to_dets = 'C:\Users\Donal\Dropbox\KITTI\output\predictions\';
-conf_thresh = 0.5;
+path_to_images = 'C:\Users\Donal\Desktop\Thesis\Data\KITTI\2011_10_03_drive_0047\';
+path_to_dets = 'C:\Users\Donal\Desktop\output\predictions\forget_squeeze\rawKITTI\2011_10_03_drive_0047\';
+conf_thresh = 0.75;
 im_size = [375, 1242];
+frame_rate = 10;
 
 % Create dictionary for plotting
 classes = {'Car', 'Van', 'Truck','Pedestrian', 'Person_sitting', 'Cyclist', 'Tram','Misc','DontCare'};
 class_num = num2cell(1:size(classes,2));
 class_dict = containers.Map(classes,class_num);
-plot_colours = ['y','m','c','r','g','b','w','k','w','k'];
+plot_colours = {'yellow','magneta','cyan','red','green','blue','white','black','white','black'};
 
 % Directories
 image_list = [dir([path_to_images, '\*.png']);dir([path_to_images, '\*.jpg'])];
 no_img = length(image_list(not([image_list.isdir])));
 det_list = dir([path_to_dets, '\*.txt']);
 
-vid = VideoWriter('C:\Users\Donal\Desktop\guau.avi');
-vid.FrameRate = 8;
+vid = VideoWriter('C:\Users\Donal\Desktop\forget_nopre_100k_2011_10_03_drive_0047.avi');
+vid.FrameRate = frame_rate;
 open(vid)
 
 for img_idx = 1:no_img
